63b7903de8d331955b5c0c0f8418377a0ab1ee25
david <f312213213david@gmail.com>
2022-05-02 17:36:10 +0800

    add loader

--- components/ContactDialog/index.jsx
+++ components/ContactDialog/index.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { ContactDialogContext } from '@/context/useContactDialog'
 
-const Index = () => {
+const ContactDialog = () => {
   const { show, toggleContactDialog } = React.useContext(ContactDialogContext)
   const [sending, setSending] = React.useState(false)
   const [responseText, setResponseText] = React.useState('')
@@ -60,7 +60,7 @@ const Index = () => {
 
   return (
       <div className={`fixed flex justify-center items-center w-full h-screen z-50 bg-gray-300 dark:bg-gray-500 bg-opacity-80 dark:bg-opacity-80 ${!show && 'hide'}`} onClick={handleClose} ref={wrapperRef}>
-        <form className="form rounded-lg bg-white dark:bg-gray-700 p-4 flex flex-col shadow-xl">
+        <form className="form w-11/12 sm:w-[32rem] rounded-lg bg-white dark:bg-gray-700 p-4 flex flex-col shadow-xl">
           <label htmlFor="name" className="text-sm text-gray-600 dark:text-gray-50 mx-4">
             Your Name
           </label>
@@ -70,6 +70,7 @@ const Index = () => {
               className="font-light rounded-md border focus:outline-none py-2 mt-2 px-1 mx-4 focus:ring-2 focus:border-none ring-blue-500"
               name="name"
               required
+              disabled={sending}
           />
           <label htmlFor="email" className="text-sm text-gray-600 dark:text-gray-50 mx-4 mt-4">
             Email
@@ -80,6 +81,7 @@ const Index = () => {
               className="font-light rounded-md border focus:outline-none py-2 mt-2 px-1 mx-4 focus:ring-2 focus:border-none ring-blue-500"
               name="email"
               required
+              disabled={sending}
           />
           <label
               htmlFor="message"
@@ -89,10 +91,11 @@ const Index = () => {
           </label>
           <textarea
               ref={messageRef}
-              rows="4"
-              className="font-light rounded-md border focus:outline-none py-2 mt-2 px-1 mx-4 focus:ring-2 focus:border-none ring-blue-500"
+              rows={5}
+              className="font-light rounded-md border focus:outline-none py-2 mt-2 px-1 mx-4 focus:ring-2 focus:border-none ring-blue-500 resize-none"
               name="message"
               required
+              disabled={sending}
           />
           <button
               onClick={sendMail}
@@ -103,7 +106,7 @@ const Index = () => {
             {
               sending
                 ? <>
-                    <svg className="animate-spin -ml-1 mr-3 h-5 w-5 text-white" xmlns="http://www.w3.org/2000/svg"
+                    <svg className="animate-spin h-5 w-5 text-white" xmlns="http://www.w3.org/2000/svg"
                          fill="none" viewBox="0 0 24 24">
                       <circle className="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" strokeWidth="4"/>
                       <path className="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"/>
@@ -118,4 +121,4 @@ const Index = () => {
   )
 }
 
-export default Index
+export default ContactDialog
