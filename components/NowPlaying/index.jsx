a1b73b774b32cf13a43fa7864825f1df75113119
david <f312213213david@gmail.com>
2022-05-03 23:55:02 +0800

    spotify RWD

--- components/NowPlaying/index.jsx
+++ components/NowPlaying/index.jsx
@@ -2,8 +2,8 @@ import React from 'react'
 
 const NowPlaying = ({ songData }) => {
   return (
-      <div className="inline-flex flex-col sm:flex-row w-full max-w-full truncate">
-        <a href={songData?.item?.external_urls?.spotify} target={'_blank'} className="capsize text-gray-800 dark:text-gray-200 font-medium" rel="noreferrer">{songData.item?.name}</a>
+      <div className="inline-flex flex-col sm:flex-row sm:w-full w-1/2 truncate">
+        <a href={songData?.item?.external_urls?.spotify} target={'_blank'} className="capsize text-gray-800 dark:text-gray-200 font-medium overflow-hidden text-ellipsis whitespace-nowrap" rel="noreferrer">{songData.item?.name}</a>
         <span className="capsize mx-2 text-gray-500 dark:text-gray-300 hidden sm:block"> – </span>
         <a href={songData.item?.artists[0]?.external_urls?.spotify} target={'_blank'} className="capsize text-gray-500 dark:text-gray-300 max-w-max truncate" rel="noreferrer">{songData.item?.artists[0]?.name}</a>
       </div>
